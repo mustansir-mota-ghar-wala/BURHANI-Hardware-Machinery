9acd719ad9957d57f29664dae0ac73254bbeca87
mustansir <mustanbhanpura87@gmail.com>
2026-06-06 01:41:10 +0530

    error resolved error 500

--- BURHANI/BURHANI/settings.py
+++ BURHANI/BURHANI/settings.py
@@ -83,6 +83,11 @@ SOCIALACCOUNT_PROVIDERS = {
 SOCIALACCOUNT_LOGIN_ON_GET = True
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
+        'APP': {
+            'client_id': os.environ.get('GOOGLE_CLIENT_ID', 'placeholder'),
+            'secret': os.environ.get('GOOGLE_CLIENT_SECRET', 'placeholder'),
+            'key': ''
+        },
         'SCOPE': [
             'profile',
             'email',
